f494f89ba561da8eef718d5652f459868cc6528e
navitha4604 <navitha462004@gmail.com>
2024-11-09 13:19:53 +0530

    Added Bootstrap setup and basic layout

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// main.js
+
+// Example: Toggle mobile navigation menu
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('active');
+  });
+  
+  // Add other event listeners and functions as needed
+  
