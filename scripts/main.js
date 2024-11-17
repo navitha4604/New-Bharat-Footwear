8732dbe2dbd1d9429626a331d67d09d5d0ef43e0
navitha4604 <146328804+navitha4604@users.noreply.github.com>
2024-11-17 18:02:25 +0530

    Delete scripts directory

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-// main.js
-
-// Example: Toggle mobile navigation menu
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-    document.querySelector('.nav-menu').classList.toggle('active');
-  });
-  
-  // Add other event listeners and functions as needed
-  
